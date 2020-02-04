b8f577606d8b4bc2a511dcdf3bea8a0308076746
Tim Levett <tlevett@sonatype.com>
2020-02-04 11:15:04 -0600

    update things

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-mkdir ~/data/mysql ~/data/prometheus ~/data/grafana
-sudo ln -s ~/data /data
-
-echo "If you are on mac, you have to add /data to File Sharing in docker settings"
+mkdir -p ./data/mysql ./data/prometheus ./data/grafana
 
 docker stack deploy -c docker-stack.yml mpg
